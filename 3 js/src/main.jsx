ac4757490ddf0eb99014435d70fdc9014e0e53b4
Gaurav Shukla <gauravshukla@Gauravs-MacBook.local>
2024-12-24 18:09:08 +0530

    update

--- 3 js/src/main.jsx
+++ 3 js/src/main.jsx
@@ -1,9 +1,19 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+
 import "./index.css";
-import App from "./App.jsx";
 import * as THREE from "three";
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
+
+const hdrLoader = new RGBELoader();
+hdrLoader.load('https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/hanger_exterior_cloudy_1k.hdr', function(texture) {
+  texture.mapping = THREE.EquirectangularReflectionMapping;
+  scene.background = texture;
+  scene.environment = texture;
+});
+
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -11,10 +21,15 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+camera.position.set(180,180,200);
+
+const loader = new GLTFLoader();
+loader.load("../range_rover.glb", (gltf) => {
+  scene.add(gltf.scene);
+});
+
+
+
 camera.position.z = 5;
 const canvas = document.querySelector("#canvas");
 const renderer = new THREE.WebGLRenderer({ canvas });
@@ -31,14 +46,6 @@ controls.enableDamping = true;
 function animate() {
   window.requestAnimationFrame(animate);
   renderer.render(scene, camera);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
   controls.update();
 }
 animate();
-
-createRoot(document.getElementById("canvas")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
